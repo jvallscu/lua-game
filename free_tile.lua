528f816090b0f190b852f08e28995f0abcb24511
jvallscu <joanvalls47@gmail.com>
2022-08-29 12:34:40 -0700

    Update

--- free_tile.lua
+++ free_tile.lua
@@ -1,5 +1,5 @@
 
-Scene = Object:extend()
+Scene = ObjectPlus:extend()
 
 frame_width = 32
 frame_height = 32
@@ -15,25 +15,16 @@ function Scene:load()
   scene = love.graphics.newImage("FD_Free_Tiles.png")
   
   frames = {}
-  
-  
-  
+
   local width = scene:getWidth()
   local height = scene:getHeight()
   
+  frames = make_frame(scene, frames, width, height)
   
   x_frame = width / frame_width
   y_frame = height / frame_height
   
-  print(x_frame)
-  print(y_frame)
   
-  
-  for i=0,x_frame - 1  do
-    for j=0,y_frame - 1  do
-      table.insert(frames, love.graphics.newQuad(i * frame_width, j * frame_height, frame_width, frame_height, width, height))
-    end
-  end
 end
 
 function Scene:update()
@@ -49,18 +40,5 @@ function Scene:draw()
 end
 
 
-function select_obj( init_x, init_y, obj_width, obj_height, x, y)
-  
-  
-  
-  for i=1,obj_width do
-    for j=1,obj_height do
-      love.graphics.draw(scene, frames[init_y + x_frame * init_x], x + i * frame_width, y + j * frame_height)
-      init_y = init_y + 1
-    end
-    
-    --Skips over to the next column of the file, making sure to keep object aligned
-    init_y = init_y + y_frame - obj_height
-  end
-  
-end
+
+
